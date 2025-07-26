464c100b397403283310ae0b22d1ada04a1ea139
Tangat Baktybergen <91661198+TangatBaktybergen@users.noreply.github.com>
2025-07-26 16:08:59 +0100

    Add files via upload

---
+++ autopdfform/db/upgrade.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Upgrade script for Auto PDF Form activity.
+ *
+ * @package    mod_autopdfform
+ * @copyright  2025 Ivan Volosyak and Tangat Baktybergen <Ivan.Volosyak@@@hochschule-rhein-waal.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+/**
+ * Handles upgrading of the mod_autopdfform module.
+ *
+ * @param int $oldversion The version we are upgrading from.
+ * @return bool True on success.
+ */
+function xmldb_autopdfform_upgrade($oldversion) {
+    global $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2025063001) {
+        $table = new xmldb_table('autopdfform');
+        $field = new xmldb_field('course', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, 0);
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Safe to try adding key without checking first.
+        $key = new xmldb_key('course', XMLDB_KEY_FOREIGN, ['course'], 'course', ['id']);
+        $dbman->add_key($table, $key);
+
+        upgrade_mod_savepoint(true, 2025063001, 'autopdfform');
+    }
+
+    return true;
+}
